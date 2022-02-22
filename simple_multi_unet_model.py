494734d1954976e375d6874373e428aac7551162
Akhil <akhilskumar120@gmail.com>
2022-02-22 15:11:22 +0530

    Updated unet_model

--- simple_multi_unet_model.py
+++ simple_multi_unet_model.py
@@ -1,11 +1,3 @@
-# https://youtu.be/jvZm8REF2KY
-"""
-Standard Unet
-Model not compiled here, instead will be done externally to make it
-easy to test various loss functions and optimizers. 
-"""
-
-
 from keras.models import Model
 from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Conv2DTranspose, BatchNormalization, Dropout, Lambda
 from keras import backend as K
@@ -16,23 +8,17 @@ def jacard_coef(y_true, y_pred):
     intersection = K.sum(y_true_f * y_pred_f)
     return (intersection + 1.0) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + 1.0)
 
-
-
-################################################################
 def multi_unet_model(n_classes=4, IMG_HEIGHT=256, IMG_WIDTH=256, IMG_CHANNELS=1):
-#Build the model
     inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
-    #s = Lambda(lambda x: x / 255)(inputs)   #No need for this if we normalize our inputs beforehand
     s = inputs
 
-    #Contraction path
     c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
-    c1 = Dropout(0.2)(c1)  # Original 0.1
+    c1 = Dropout(0.2)(c1)  
     c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
     p1 = MaxPooling2D((2, 2))(c1)
     
     c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
-    c2 = Dropout(0.2)(c2)  # Original 0.1
+    c2 = Dropout(0.2)(c2)  
     c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
     p2 = MaxPooling2D((2, 2))(c2)
      
@@ -50,7 +36,6 @@ def multi_unet_model(n_classes=4, IMG_HEIGHT=256, IMG_WIDTH=256, IMG_CHANNELS=1)
     c5 = Dropout(0.3)(c5)
     c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
     
-    #Expansive path 
     u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
     u6 = concatenate([u6, c4])
     c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
@@ -66,22 +51,16 @@ def multi_unet_model(n_classes=4, IMG_HEIGHT=256, IMG_WIDTH=256, IMG_CHANNELS=1)
     u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
     u8 = concatenate([u8, c2])
     c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
-    c8 = Dropout(0.2)(c8)  # Original 0.1
+    c8 = Dropout(0.2)(c8)  
     c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
      
     u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
     u9 = concatenate([u9, c1], axis=3)
     c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
-    c9 = Dropout(0.2)(c9)  # Original 0.1
+    c9 = Dropout(0.2)(c9)  
     c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
      
     outputs = Conv2D(n_classes, (1, 1), activation='softmax')(c9)
-     
     model = Model(inputs=[inputs], outputs=[outputs])
-    
-    #NOTE: Compile the model in the main program to make it easy to test with various loss functions
-    #model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-    
-    #model.summary()
     
     return model
